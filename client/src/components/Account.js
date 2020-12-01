0958c9c685e9f3b378b3e1c5bb3e482c5469fd01
jdcheek <58921502+jdcheek@users.noreply.github.com>
2020-11-30 22:22:49 -0600

    added basic account details

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
+import axios from "axios";
+import { UserContext } from "./UserContext";
 
 const Account = () => {
-  return (
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const mountedRef = useRef(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [account, setAccount] = useState({
+    username: "",
+    toolsCheckedOut: [],
+  });
+
+  const getAccountInfo = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/auth/status/`, {
+        withCredentials: true,
+      });
+      if (mountedRef.current) {
+        setAccount({
+          username: res.data.username,
+          toolsCheckedOut: res.data.toolsCheckedOut,
+        });
+        setIsLoading(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(account);
+  useEffect(() => {
+    getAccountInfo();
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
     <div>
       <h2>Account</h2>
-      <p>Username: jcheek</p>
+      <p>Username: {account.username}</p>
       <p>Change Password</p>
       <h4>Tools Checked Out</h4>
       <ul>
-        <li>J-1234</li>
-        <li>J-2234</li>
-        <li>J-3234</li>
-        <li>J-4234</li>
-        <li>J-5234</li>
+        {account.toolsCheckedOut.map((tool) => (
+          <li key={Math.random()}>
+            Tool: {tool.tool_number} Location: {tool.location.bin}-
+            {tool.location.shelf}
+          </li>
+        ))}
       </ul>
       <p>Coming Soon...</p>
       <p>Report Damaged Tool</p>
     </div>
   );
-}
+};
 
-export default Account
+export default Account;
