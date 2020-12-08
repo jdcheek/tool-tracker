6abba26adbb0ca9580c807728c0236ebfb6ad2f8
jdcheek <58921502+jdcheek@users.noreply.github.com>
2020-12-07 23:33:29 -0600

    updated server

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "..", "/client", "/build")));
-app.use(cors({ origin: `/`, credentials: true }));
+app.use(
+  cors({
+    origin: "https://shielded-mesa-30476.herokuapp.com/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
