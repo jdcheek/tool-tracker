c057ada91d4918c0edeeff199344592455e89c2a
jdcheek <58921502+jdcheek@users.noreply.github.com>
2020-12-07 21:17:44 -0600

    added path to serve index

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const app = express();
+const path = require("path");
+const publicPath = path.join(__dirname, "../client", "build");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const inventoryRouter = require("./routes/inventory.routes");
 const usersRouter = require("./routes/user.routes");
-const authRouter = require("./routes/auth.routes")
-//TODO import Helmet
+const authRouter = require("./routes/auth.routes");
 
 require("dotenv").config();
 
-const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: "*", credentials: false }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
@@ -29,13 +35,13 @@ db.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-db.on('error', (error) => {
+db.on("error", (error) => {
   console.log(error);
-})
+});
 
 app.use("/inventory", inventoryRouter);
 app.use("/user", usersRouter);
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
