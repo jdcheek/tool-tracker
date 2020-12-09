09d506dc1722da296be01138f04a45a3886de24a
jdcheek <58921502+jdcheek@users.noreply.github.com>
2020-12-08 20:46:34 -0600

    updated server

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
