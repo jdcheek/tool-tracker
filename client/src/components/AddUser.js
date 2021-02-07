31ec776254eddc4a9b9de0b9ee63099e945db914
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-02-07 11:43:49 -0600

    reworked add user component

--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -74,60 +74,6 @@ const AddUser = ({ getUsers, isLoading }) => {
       </Button>
       {error ? <h3>{error}</h3> : null}
     </Form>
-    // <div>
-    //   <div>
-    //     <h2>Create New User</h2>
-    //   </div>
-    //   <form onSubmit={onAddSubmit}>
-    //     <div className="form-group">
-    //       <label htmlFor="username">Username</label>
-    //       <input
-    //         type="text"
-    //         required
-    //         className="form-control"
-    //         value={newUser.username}
-    //         onChange={(e) => {
-    //           setNewUser({ ...newUser, username: e.target.value });
-    //         }}
-    //       />
-    //     </div>
-    //     <div className="form-group">
-    //       <label htmlFor="description">Password</label>
-    //       <input
-    //         type="text"
-    //         required
-    //         className="form-control"
-    //         value={newUser.password}
-    //         onChange={(e) => {
-    //           setNewUser({ ...newUser, password: e.target.value });
-    //         }}
-    //       />
-    //     </div>
-    //     <div className="form-group">
-    //       <label htmlFor="description">Retype Password</label>
-    //       <input
-    //         type="text"
-    //         required
-    //         className="form-control"
-    //         value={newUser.retypedPassword}
-    //         onChange={(e) => {
-    //           setNewUser({ ...newUser, retypedPassword: e.target.value });
-    //         }}
-    //       />
-    //     </div>
-    //     <div>
-    //       {newUser.username.length < 3 ? (
-    //         <p>Username must be at least 3 characters</p>
-    //       ) : newUser.password.length < 8 ? (
-    //         <p>Password must be at least 8 characters</p>
-    //       ) : newUser.password !== newUser.retypedPassword ? (
-    //         <p>Passwords do not match</p>
-    //       ) : (
-    //               <button onClick={onAddSubmit}>Add New User</button>
-    //             )}
-    //     </div>
-    //   </form>
-    // </div>
   );
 };
 
