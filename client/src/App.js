589156fabe561e1f9e383bd6a33d71a768455eed
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-02-21 21:21:36 -0600

    moved reused function passed as props

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import axios from "axios";
 import Account from "./components/Account";
 import AddUser from "./components/AddUser";
 import Dashboard from "./components/Dashboard";
@@ -13,28 +13,58 @@ import Navigation from "./components/Navigation";
 import LogIn from "./components/LogIn";
 
 function App() {
+  const mountedRef = useRef(true);
   const [currentUser, setCurrentUser] = useState({
     isLoggedIn: false,
     isAdmin: false,
     username: null,
+    toolsCheckedOut: [],
   });
+
+  const getAccountInfo = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/auth/status/`, {
+        withCredentials: true,
+      });
+
+      if (mountedRef.current) {
+        setCurrentUser(res.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className='App'>
-      <Router>
-        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-          <Navigation />
-          <main className='app-container'>
-            <Route exact path='/tools' component={Inventory} />
-            <Route exact path='/login' component={LogIn} />
-            <Route exact path='/account' component={Account} />
-            <Route exact path='/tools/edit' component={EditInventory} />
-            <Route exact path='/user/add' component={AddUser} />
-            <Route exact path='/user/edit' component={EditUser} />
-            <Route exact path='/dashboard' component={Dashboard} />
-          </main>
-        </UserContext.Provider>
-      </Router>
+      <div className='wrapper'>
+        <Router>
+          <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+            <Navigation />
+            <div className='app-container'>
+              {currentUser.isLoggedIn && (
+                <section className='side-bar'>
+                  <Account getAccountInfo={getAccountInfo} />
+                </section>
+              )}
+              <main className='page-container'>
+                <Route
+                  exact
+                  path='/tools'
+                  component={() => (
+                    <Inventory getAccountInfo={getAccountInfo} />
+                  )}
+                />
+                <Route exact path='/login' component={LogIn} />
+                <Route exact path='/tools/edit' component={EditInventory} />
+                <Route exact path='/user/add' component={AddUser} />
+                <Route exact path='/user/edit' component={EditUser} />
+                <Route exact path='/dashboard' component={Dashboard} />
+              </main>
+            </div>
+          </UserContext.Provider>
+        </Router>
+      </div>
     </div>
   );
 }
