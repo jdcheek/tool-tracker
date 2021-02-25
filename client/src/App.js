580241faac8bac134de0409d735286532963fd08
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-02-24 21:03:08 -0600

    removed useEffect and unused function

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import LogIn from "./components/LogIn";
 
 function App() {
   const mountedRef = useRef(true);
+
   const [currentUser, setCurrentUser] = useState({
     isLoggedIn: false,
     isAdmin: false,
