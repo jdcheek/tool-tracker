9f5e62cd1ce3750f77e64ba3fd791b32d1bb90e6
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-03-01 22:36:14 -0600

    added delete route

--- server/routes/inventory.routes.js
+++ server/routes/inventory.routes.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { getInventory, createInventory, getInventoryById, deleteInventory, updateInventory, updateStatus } = require("../controllers/inventory.controller");
+const {
+  getInventory,
+  createInventory,
+  getInventoryById,
+  deleteInventory,
+  updateInventory,
+  updateStatus,
+} = require("../controllers/inventory.controller");
 const router = express.Router();
 const Inventory = require("../models/inventory.model");
-const auth = require('../middleware/auth')
-const user = require('../middleware/user')
+const auth = require("../middleware/auth");
+const user = require("../middleware/user");
 
 router.get("/", getInventory);
 router.post("/add", auth, createInventory);
 router.get("/:id", auth, getInventoryById);
-router.delete("/:id", auth, deleteInventory);
+router.delete("/delete/:id", auth, deleteInventory);
 router.post("/update/:id", user, updateInventory);
 router.post("/update/status/:id", user, updateStatus);
 
