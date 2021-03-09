51bf8c27992a9b2c63f54cfbba1834422046f5d8
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-03-08 21:40:03 -0600

    created user management modal

---
+++ client/src/components/modals/AddUserModal.js
@@ -0,0 +1,226 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Button, Modal, InputGroup, Form, Alert } from "react-bootstrap";
+import axios from "axios";
+
+const AddUserModal = (props) => {
+  const { ...rest } = props;
+  const mountedRef = useRef(true);
+  const [error, setError] = useState("");
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const clearForm = {
+    username: "",
+    password: "",
+    retypedPassword: "",
+    isAdmin: false,
+  };
+  const [user, setUser] = useState(clearForm);
+  const [userID, setUserID] = useState("");
+  const handleCancel = () => {
+    setUser(clearForm);
+    setUserID("");
+  };
+
+  const getUsers = async () => {
+    let list = [];
+    try {
+      const res = await axios.get("http://localhost:5000/user", {
+        withCredentials: true,
+      });
+      res.data.map((user) =>
+        list.push({
+          username: user.username,
+          _id: user._id,
+          isAdmin: user.isAdmin,
+        })
+      );
+      if (mountedRef.current) {
+        setUserList(list);
+        setIsLoading(false);
+      }
+    } catch (err) {
+      console.log(`Get users error: ${err}`);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/user/add", user, {
+        withCredentials: true,
+      });
+      setUser(clearForm);
+      getUsers();
+      return res;
+    } catch (err) {
+      if (err.response.data) {
+        setError({ message: err.response.data });
+        setTimeout(() => setError(""), 3000);
+      }
+      console.log(err.response.data);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:5000/user/delete/${userID}`,
+        { withCredentials: true }
+      );
+      setUserList(() => userList.filter((user) => user._id !== userID));
+    } catch (err) {
+      if (err.response.data) {
+        setError({ message: err.response.data });
+        setTimeout(() => setError(""), 3000);
+      }
+      console.log(err.response.data);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  return (
+    <Modal
+      {...rest}
+      size='md'
+      aria-labelledby='contained-modal-title-vcenter'
+      centered>
+      <Modal.Header
+        closeButton
+        onClick={() => {
+          handleCancel();
+        }}>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          Manage Users
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group style={{ width: "100%" }}>
+            <Form.Label>Select User</Form.Label>
+            <Form.Control as='select' htmlSize={3} custom>
+              {userList.map((user) => (
+                <option
+                  onClick={(e) => setUserID(e.target.value)}
+                  key={user._id}
+                  value={user._id}>
+                  {user.username}
+                </option>
+              ))}
+            </Form.Control>
+            {userID && (
+              <Button
+                variant='outline-danger'
+                onClick={() => {
+                  handleDelete();
+                }}>
+                Delete
+              </Button>
+            )}
+          </Form.Group>
+        </Form>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          Create User
+        </Modal.Title>
+        {error.message && (
+          <Alert key={error} variant='danger'>
+            {error.message}
+          </Alert>
+        )}
+        <InputGroup className='mb-3'>
+          <InputGroup.Prepend>
+            <InputGroup.Text style={{ width: "150px" }}>
+              Username
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            isValid={user.username.length >= 3 || ""}
+            isInvalid={user.username.length < 3 && user.username !== ""}
+            required
+            type='text'
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+          <Form.Control.Feedback type='invalid'>
+            Username must be greater than 3 characters
+          </Form.Control.Feedback>
+        </InputGroup>
+        <InputGroup className='mb-3'>
+          <InputGroup.Prepend>
+            <InputGroup.Text style={{ width: "150px" }}>
+              Password
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            isValid={user.password.length >= 8}
+            isInvalid={user.password.length < 8 && user.password !== ""}
+            type='password'
+            required
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+          <Form.Control.Feedback type='invalid'>
+            Password must be greater than 8 characters
+          </Form.Control.Feedback>
+        </InputGroup>
+        <InputGroup className='mb-3'>
+          <InputGroup.Prepend>
+            <InputGroup.Text style={{ width: "150px" }}>
+              Retype Password
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type='password'
+            required
+            isValid={
+              user.password === user.retypedPassword &&
+              user.retypedPassword !== ""
+            }
+            isInvalid={user.password !== user.retypedPassword}
+            value={user.retypedPassword || ""}
+            onChange={(e) =>
+              setUser({ ...user, retypedPassword: e.target.value })
+            }
+          />
+          <Form.Control.Feedback type='invalid'>
+            Passwords do not match
+          </Form.Control.Feedback>
+        </InputGroup>
+
+        <Form>
+          <Form.Check
+            type='switch'
+            checked={user.isAdmin}
+            onChange={() => setUser({ ...user, isAdmin: !user.isAdmin })}
+            id='admin'
+            label='Administrative Access'
+          />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant='outline-primary'
+          onClick={() => {
+            handleSubmit();
+          }}>
+          Submit
+        </Button>
+
+        <Button
+          variant='outline-dark'
+          onClick={() => {
+            props.onHide();
+            handleCancel();
+          }}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default AddUserModal;
