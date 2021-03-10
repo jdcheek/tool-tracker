27b3edf83b5eff616c7ae2cb18d248622ecdf064
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-03-09 21:14:07 -0600

    shortened pagination menu

--- client/src/components/lib/Pagination.js
+++ client/src/components/lib/Pagination.js
@@ -1,16 +1,7 @@
 import React from "react";
 import { Pagination } from "react-bootstrap";
 
-const PaginationComponent = ({
-  setItemsPerPage,
-  paginate,
-  currentPage,
-  pages,
-}) => {
-  const setPaginateToInnerText = (e) => {
-    paginate(parseInt(e.target.innerText));
-  };
-
+const PaginationComponent = ({ paginate, currentPage, pages }) => {
   return (
     <div className='footer'>
       <Pagination>
@@ -27,45 +18,9 @@ const PaginationComponent = ({
             paginate(currentPage - 1);
           }}
         />
-
-        {currentPage > 1 && (
-          <>
-            <Pagination.Item onClick={(e) => setPaginateToInnerText(e)}>
-              {1}
-            </Pagination.Item>
-            <Pagination.Ellipsis />
-          </>
-        )}
-
-        {currentPage > 3 && (
-          <Pagination.Item onClick={(e) => setPaginateToInnerText(e)}>
-            {currentPage - 2}
-          </Pagination.Item>
-        )}
-        {currentPage > 2 && (
-          <Pagination.Item onClick={(e) => setPaginateToInnerText(e)}>
-            {currentPage - 1}
-          </Pagination.Item>
-        )}
-        <Pagination.Item active>{currentPage}</Pagination.Item>
-        {currentPage <= pages - 1 && (
-          <Pagination.Item onClick={(e) => setPaginateToInnerText(e)}>
-            {currentPage + 1}
-          </Pagination.Item>
-        )}
-        {currentPage <= pages - 2 && (
-          <Pagination.Item onClick={(e) => setPaginateToInnerText(e)}>
-            {currentPage + 2}
-          </Pagination.Item>
-        )}
-        {currentPage <= pages - 20 && (
-          <>
-            <Pagination.Ellipsis />
-            <Pagination.Item onClick={(e) => setPaginateToInnerText(e)}>
-              {currentPage === 1 ? 20 : currentPage + 20}
-            </Pagination.Item>
-          </>
-        )}
+        <Pagination.Item active>
+          {currentPage} of {pages || 1}
+        </Pagination.Item>
         <Pagination.Next
           disabled={currentPage === pages}
           onClick={() => {
